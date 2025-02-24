bd8fd2b53f3befd879d7b701492798a4a0d34255
Uvindu Amaratunga <83293493+UvinduRokz@users.noreply.github.com>
2025-02-24 09:15:26 +0530

    updated: vite config to match varcel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';  // Adjust the path if needed
-import './App.css';
+import Home from "./pages/Home"; // Adjust the path if needed
+import "./App.css";
 
 function App() {
-    return (
-        <Router basename="/SlotZi-marketing/">
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router basename="/SlotZi-marketing">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
