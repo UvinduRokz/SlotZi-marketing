d99c82d7136bcee496a837e81095d7550dead0aa
Uvindu Amaratunga <83293493+UvinduRokz@users.noreply.github.com>
2025-02-24 09:18:27 +0530

    updated: base URL and asset paths for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"; // Adjust the path if needed
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
-    <Router basename="/SlotZi-marketing">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
