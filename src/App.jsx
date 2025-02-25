713db6f200b31f5659aa363b1ca6e7b7de6fd40e
Uvindu Amaratunga <83293493+UvinduRokz@users.noreply.github.com>
2025-02-25 09:03:45 +0530

    fix: App.jsx and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/home';  // Adjust the path if needed
-import './App.css';
+import Home from "./pages/Home";
+import "./App.css";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                {/* Add more routes as needed */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
